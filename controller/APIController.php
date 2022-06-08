ad97712aca82a0aaf435a6f30698f3aaa824b486
Benjamin <benjamin.lariotte@outlook.fr>
2022-06-08 13:29:59 +0200

    choses en cours

--- controller/APIController.php
+++ controller/APIController.php
@@ -5,8 +5,31 @@ class APIController extends Controller
 {
     public static function researchProduct($name)
     {
-        $result = file_get_contents("https://fr.openfoodfacts.org/api/v2/search?categories_tags_fr=".$name."&fields=code,product_name,origin_fr,nutrition_grade_fr,allergens,stores,image_url");
+        $result = file_get_contents("https://fr.openfoodfacts.org/api/v2/search?categories_tags_fr=".$name."&fields=code,product_name,image_url,origin_fr,nutrition_grade_fr,allergens,stores");
 
         return $result;
+    }
+
+    public static function compareProductPopularity($productCode1, $productCode2)
+    {
+        $product1Popularity = json_decode(file_get_contents("https://fr.openfoodfacts.org/api/v2/search?code=".$productCode1."&fields=popularity_tags"));
+
+        $product2Popularity = file_get_contents("https://fr.openfoodfacts.org/api/v2/search?code=".$productCode2."&fields=popularity_tags");
+
+        var_dump(json_decode($product1Popularity));
+        $product1Popularity = (array)$product1Popularity->product;
+
+
+        exit();
+
+        foreach ($product1Popularity as $value)
+        {
+            preg_match("2022", $value, $result);
+            return $result;
+        }
+
+        $test = [$product1Popularity, $product2Popularity];
+
+        return $test;
     }
 }
