d5b555861e17ea3a8648d0dad2dde702aeaa5306
Dorian <dorian.bermond@gmail.com>
2022-06-08 11:12:55 +0200

    OFF API

--- models/ErrorClass.php
+++ models/ErrorClass.php
@@ -1,9 +1,18 @@
 <?php
 
+define("ALLOWED_TYPES", ["error", "warning", ]);
+
 class ErrorResponse {
     private $errorTitle ;
     private $errorMessage ;
+    private $errorType ;
     private $other ;
+
+    public function checkValues() {
+        if in_array(strtolower($this->$errorType), ALLOWED_TYPES) {
+            
+        }
+    }
 
     public function __construct($errorTtl, $errorMsg, $oth = null) {
         $this->errorTitle = $errorTtl ;
@@ -25,6 +34,14 @@ class ErrorResponse {
 
     public function setErrorMessage($input) {
         $this->errorMessage = $input ;
+    }
+
+    public function getOther() {
+        return $this->other ;
+    }
+
+    public function setOther($input) {
+        $this->other = $input ;
     }
 
     public function getOther() {
